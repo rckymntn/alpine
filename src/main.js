cb0a936a8f7d76754f64a18e24050029e5188093
Logan Raffkind <logan.raffkind@gmail.com>
2022-01-31 19:36:30 -0800

    Handling once and on interactions

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
 require("dotenv").config();
-const {Client, Intents} = require("discord.js"); 
+const fs = require("fs");
+const {Client, Collection, Intents} = require("discord.js"); 
+
 
 const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
+client.commands = new Collection();
+const commandList = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js"));
+for (const file of commandList) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+
+
 client.once("ready", () => {
     console.log("ready");
+});
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isCommand()) {
+        return;
+    } else {
+        const command = client.commands.get(interaction.commandName);
+        if (!command) {
+            return;
+        }
+        try {
+            await command.execute(interaction);
+        } catch(error) {
+            console.log(error);
+            await interaction.reply({ content: "Error.", ephemeral: true});
+        }
+    }
 });
 
 client.login(token);
