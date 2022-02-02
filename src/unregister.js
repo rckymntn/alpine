681586c0bdf53ec14cb5ff1b2aab24c7573da96d
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-01 19:46:57 -0800

    Create unregister.js

---
+++ src/unregister.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN);
+
+rest.get(Routes.applicationCommands(process.env.CLIENT_ID)).then(data => {
+    const promises = [];
+    for (const command of data) {
+        const unregister = `${Routes.applicationCommands(process.env.CLIENT_ID)}/${command.id}`;
+        promises.push(rest.delete(unregister));
+    }
+    return Promise.all(promises);
+});
