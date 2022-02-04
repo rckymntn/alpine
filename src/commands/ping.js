683a2957d0f24c3bba41cf86f8d4bf4c64922127
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-04 14:37:22 -0800

    Changed whitespace to be more consistent

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -6,7 +6,10 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    data: new SlashCommandBuilder().setName("ping").setDescription("pong"), async execute(interaction) {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("pong"),
+    async execute(interaction) {
         await interaction.reply("pong");
     }
 };
