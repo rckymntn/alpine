f5505a7d31337e189a76b792b87c0356125a52ba
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-15 13:02:49 -0800

    Added printing to console a list of servers using the bot

--- src/main.js
+++ src/main.js
@@ -34,7 +34,8 @@ for (const file of commandList) {
  *  This will run once at startup. 
  */
 client.once("ready", () => {
-    console.log(`${client.user.username} ready`);
+    console.log(`${client.user.username} ready.`);
+    client.guilds.cache.forEach(guild => console.log(`Serving ${guild.name}.`));
     client.user.setPresence("online");
     //commandManager.unregister();
     commandManager.register(); 
