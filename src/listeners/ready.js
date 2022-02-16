c2c733cdd296bf9c8077aec66b884c4d4bc4f07e
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-16 13:42:23 -0800

    Updated the ready listener file

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -2,9 +2,10 @@ const commandManager = require("../command-manager");
 
 module.exports.ready = (client) => {
     client.once("ready", () => {
-        console.log("Ready.");
-        client.user.setPresence("online");   
-        commandManager.unregister();
+        console.log(`${client.user.username} ready.`);
+        client.guilds.cache.forEach(guild => console.log(`Serving ${guild.name}.`));
+        client.user.setPresence("online");
+        //commandManager.unregister();
         commandManager.register(); 
     });
 }
