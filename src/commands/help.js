b15e47cafd08030761dfc68863a0989716c33f2a
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-15 19:24:02 -0800

    Changed whitespacing for long interaction response

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,6 +11,10 @@ module.exports = {
         .setDefaultPermission(true),
     async execute(interaction) {
         const bot = interaction.client.user.username;
-        await interaction.reply(`${bot} is an open source, multipurpose Discord bot written in JavaScript.`);
+        await interaction.reply(
+            `${bot} is an open source, multipurpose Discord bot written in JavaScript. 
+            Use slash commands to interact with the bot. 
+            For a list of commands, type "/" and navigate to the ${bot} tab.`
+        );
     }
 };
