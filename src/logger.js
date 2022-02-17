53df1f3f2258a947cec9c2b758b55ba3f16ede71
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-17 15:28:14 -0800

    Added to the TODO as a note for later

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,13 @@
-module.exports.logger = (string) => {
+/*
+ *  Logs information to a variety of places.
+ *  
+ *  string: String to be logged
+ *  context: Message or interaction to get the guild it originated from 
+ */
+module.exports.logger = (string, context) => {
     console.log(string);
-    //TODO: Write string to a log file 
+    // TODO: Write string to a log file 
+    // In the future, you could also pass the message or interaction (contex)
+    // as a parameter to get the guild in which it was sent and send the
+    // log to a dedicated log channel in that guild
 }
