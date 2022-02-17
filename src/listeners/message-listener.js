9247094f8ea59da1721c3ce4ff3ffd8ed22e457b
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-17 15:39:53 -0800

    Added messageCreate, messageUpdate, and messageDelete to message-listener

--- src/listeners/message-listener.js
+++ src/listeners/message-listener.js
@@ -1,11 +1,36 @@
+/*
+ *  This will run on every new message sent in a server. 
+ */
 module.exports.messageCreate = (client) => {
-
+    client.on("messageCreate", async message => {
+        if (message.author.bot) {
+            return;
+        }
+        console.log(`${message.author.username} sent message "${message.content}"`);
+    });
 }
 
+
+/*
+ *  This will run on every message update in a server. 
+ */
 module.exports.messageUpdate = (client) => {
-
+    client.on("messageUpdate", async message => {
+        if (message.author.bot) {
+            return;
+        }
+        console.log(`${message.author.username} edited message "${message}"`);
+    });
 }
 
+/*
+ *  This will run on every message delete in a server. 
+ */
 module.exports.messageDelete = (client) => {
-
+    client.on("messageDelete", async message => {
+        if (message.author.bot) {
+            return;
+        }
+        console.log(`${message.author.username} deleted message "${message}"`);
+    });
 }
