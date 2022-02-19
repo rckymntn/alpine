3447c864595f5392860ec0712c44f0fef524e3c5
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-18 18:38:15 -0800

    Created separate functions for different logging destinations

--- src/lumberjack.js
+++ src/lumberjack.js
@@ -1,13 +1,32 @@
 /*
- *  Logs information to a variety of places.
- *  
- *  string: String to be logged
- *  context: Message or interaction to get the guild it originated from 
+ *  Logs a string to console
+ */
+module.exports.consoleLogger = (string, context) => {
+    console.log(string);
+}
+
+
+/*
+ *  Logs a string to a channel
+ */
+module.exports.channelLogger = (string, context) => {
+    // Write log to log channel
+}
+
+
+/*
+ *  Logs a string to a file
+ */
+module.exports.fileLogger = (string, context) => {
+    // Write log to log file
+}
+
+
+/*
+ *  Logs a string to console, channel, and file 
  */
 module.exports.logger = (string, context) => {
-    console.log(string);
-    // TODO: Write string to a log file 
-    // In the future, you could also pass the message or interaction (contex)
-    // as a parameter to get the guild in which it was sent and send the
-    // log to a dedicated log channel in that guild
+    this.consoleLogger(string, context);
+    this.channelLogger(string, context);
+    this.fileLogger(string, context);
 }
