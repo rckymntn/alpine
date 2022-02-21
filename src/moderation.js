fbc9db356a1f7174e9a1d276eb53beaa5b58a976
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-21 09:22:26 -0800

    Implemented filter and sanitization

--- src/moderation.js
+++ src/moderation.js
@@ -1,7 +1,22 @@
+const fs = require("fs");
+
+
+const filterArray = fs.readFileSync("./filter.csv", "utf-8").split(",");
+
+
 /*
- *  Returns true if a word in message matches a word in filter.txt
- *  Returns false otherwise 
+ *  Returns true if a word in message matches a word in filter.csv, false otherwise
  */
 module.exports.filter = (message) => {
+    for (i = 0; i < filterArray.length; i++) {
+        if (sanitize(String(message)).includes(sanitize(filterArray[i]))) {
+            return true;
+        }
+    }
     return false;
+}
+
+
+function sanitize(string) {
+    return string.toLowerCase().replace(/['".,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 }
