8bc0c41477cb7e4d67e059becb7e4e28deb730b9
Logan Raffkind <logan.raffkind@gmail.com>
2022-02-25 12:47:24 -0800

    Create invite-listener.js

---
+++ src/listeners/invite-listener.js
@@ -0,0 +1,12 @@
+module.exports.inviteCreate = (client) => {
+    client.on("inviteCreate", async invite => {
+
+    });
+}
+
+
+module.exports.inviteDelete = (client) => {
+    client.on("inviteDelete", async invite => {
+
+    });
+}
