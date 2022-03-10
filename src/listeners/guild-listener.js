b774cbfa8d2cf75ee2e424d1df8986c4a741f8ff
Logan Raffkind <logan.raffkind@gmail.com>
2022-03-10 14:07:32 -0800

    Added guildRemberRemove listener

---
+++ src/listeners/guild-listener.js
@@ -0,0 +1,10 @@
+const lumberjack = require("../lumberjack");
+
+/*
+ *
+ */
+module.exports.guildMemberRemove = (client) => {
+    client.on("guildMemberRemove", async member => {
+        
+    });
+}
