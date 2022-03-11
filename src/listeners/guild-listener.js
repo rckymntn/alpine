1c6deb58f30e00275b5b96b60a3a3e5d76c035e8
Logan Raffkind <logan.raffkind@gmail.com>
2022-03-11 12:59:08 -0800

    Logging for member leaving and member being kicked

--- src/listeners/guild-listener.js
+++ src/listeners/guild-listener.js
@@ -5,6 +5,14 @@ const lumberjack = require("../lumberjack");
  */
 module.exports.guildMemberRemove = (client) => {
     client.on("guildMemberRemove", async member => {
-        
+        const auditLog = await member.guild.fetchAuditLogs({limit: 1, type: "MEMBER_KICK"}).entries.first();
+        const {kicker, kickee} = auditLog;
+        if (!auditLog) {
+            lumberjack.consoleLogger(`${member.user.tag} left ${client.guild.id}`);
+        } else if (kickee.id == member.id) {
+            lumberjack.consoleLogger(`${member.user.tag} was kicked from ${client.guild.id} by ${kickee.user.tag}`);
+        } else {
+            lumberjack.consoleLogger(`${member.user.tag} left ${client.guild.id}`);
+        }
     });
 }
