7b7e9a430d96694a6c2ba30a37561f2dc62516c8
Logan Raffkind <logan.raffkind@gmail.com>
2022-03-16 18:06:05 -0700

    Command setup

---
+++ src/commands/uwu.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("uwu")
+        .setDescription("uwu-ifies text")
+        .setDefaultPermission(true)
+        .addStringOption(option => option
+            .setName("text")
+            .setDescription("text to uwu-ify")
+            .setRequired(true)),
+    async execute(interaction) {
+        await interaction.reply(``);
+    }
+}
