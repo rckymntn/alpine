fc232e63ab856a6def12261b9d4ce1dd89405293
Logan Raffkind <logan.raffkind@gmail.com>
2022-03-23 09:41:00 -0700

    Added header comments to different tests

--- tests/lumberjack-test.js
+++ tests/lumberjack-test.js
@@ -1,16 +1,30 @@
+/*
+ *  Tests for lumberjack.js
+ */
+
 const lumberjack = require("../src/lumberjack");
 
 
+/*
+ *  lumberjack.fileLogger with a string provided, but no context
+ */
 function lumberjackFileLoggerNoContextTest() {
     lumberjack.fileLogger("No context test");
 }
 
 
+/*
+ *  lumberjack.fileLogger with no string and no context provided
+ */
 function lumberjackFileLoggerNoContextNoStringTest() {
     lumberjack.fileLogger();
 }
 
-function lumberjackFileLoggerNthTest(iterations) {
+
+/*
+ *  lumberjack.fileLogger with no context provided for a certain amount of iterations
+ */
+function lumberjackFileLoggerNoContextNthTest(iterations) {
     for (i = 0; i < iterations; i++) {
         lumberjack.fileLogger(`lumberjackFileLoggerNthTest iteration ${i}`);
     }
@@ -19,4 +33,4 @@ function lumberjackFileLoggerNthTest(iterations) {
 
 lumberjackFileLoggerNoContextTest();
 lumberjackFileLoggerNoContextNoStringTest();
-lumberjackFileLoggerNthTest(10);
+lumberjackFileLoggerNoContextNthTest(10);
