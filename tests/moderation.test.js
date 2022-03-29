2f3d83592b8d324777dc486bc7f1c4848a4b0b39
Logan Raffkind <logan.raffkind@gmail.com>
2022-03-29 13:37:14 -0700

    Wrote more tests for filter

--- tests/moderation.test.js
+++ tests/moderation.test.js
@@ -7,16 +7,36 @@ const moderation = require("../src/moderation");
 
 
 /*
- *  Test filter with an empty string
+ *  Test filter with a null string
  */
-test("filter with an empty string", () => {
-    expect(moderation.filter("")).toBe(false);
+test("filter with a null string", () => {
+    const string = null;
+    expect(moderation.filter(string)).toBe(false);
 });
 
 
 /*
- *  Test filter with a null string
+ *  Test filter with an empty string
  */
-test("filter with a null string", () => {
-    expect(moderation.filter(null)).toBe(false);
+test("filter with an empty string", () => {
+    const string = "";
+    expect(moderation.filter(string)).toBe(false);
+});
+
+
+/* 
+ *  Test filter with an integer
+ */
+test("filter with an integer", () => {
+    const string = 1;
+    expect(moderation.filter(string)).toBe(false);
+})
+
+
+/* 
+ *  Test filter with no blocked terms
+ */
+test("filter with no non-permitted terms", () => {
+    const string = "The quick brown fox leaped over the lazy dog";
+    expect(moderation.filter(string)).toBe(false);
 });
