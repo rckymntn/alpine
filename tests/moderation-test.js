f5794e74a555034442804a6b9566fe40bda76562
Logan Raffkind <logan.raffkind@gmail.com>
2022-03-28 19:35:24 -0700

    Created filterNullStringTest()

--- tests/moderation-test.js
+++ tests/moderation-test.js
@@ -13,9 +13,21 @@ function filterEmptyStringTest() {
     const string = "";
     const expected = false;
     const actual = moderation.filter(string);
-    result = expected === actual;
+    const result = expected === actual;
+    console.log(`filterEmptyStringTest - Expected: ${expected} Actual: ${actual}`);
+}
+
+/*
+ *  Test filter with a null string
+ */
+function filterNullStringTest() {
+    const string = null;
+    const expected = false;
+    const actual = moderation.filter(string);
+    const result = expected === actual;
     console.log(`filterEmptyStringTest - Expected: ${expected} Actual: ${actual}`);
 }
 
 
 filterEmptyStringTest();
+filterNullStringTest();
