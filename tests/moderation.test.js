fd4f375e647440527ebf92eaf17b8eca4f8a9935
Logan Raffkind <logan.raffkind@gmail.com>
2022-03-30 10:27:08 -0700

    Added two more tests to moderation.test.js

--- tests/moderation.test.js
+++ tests/moderation.test.js
@@ -4,6 +4,7 @@
 
 
 const moderation = require("../src/moderation");
+const fs = require("fs");
 
 
 /*
@@ -39,4 +40,27 @@ test("filter with an integer", () => {
 test("filter with no non-permitted terms", () => {
     const string = "The quick brown fox leaped over the lazy dog";
     expect(moderation.filter(string)).toBe(false);
+});
+
+
+/*
+ *  Test filter with one blocked term selected randomly from filter.csv
+ */
+test("filter with one random non-permitted term", () => {
+    const filterArray = fs.readFileSync("./filter.csv", "utf-8").split(",");
+    const randWord = filterArray[Math.floor(Math.random() * filterArray.length)];
+    const string = `The quick brown fox ${randWord} over the lazy dog`;
+    expect(moderation.filter(string)).toBe(true);
+});
+
+
+/*
+ *
+ */
+test("filter with one random obfuscated non-permitted term", () => {
+    const filterArray = fs.readFileSync("./filter.csv", "utf-8").split(",");
+    const randWord = filterArray[Math.floor(Math.random() * filterArray.length)];
+    const randWordObfuscated = randWord.slice(0, 1) + "." + randWord.slice(1);
+    const string = `The quick brown fox ${randWordObfuscated} over the lazy dog`;
+    expect(moderation.filter(string)).toBe(true);
 });
