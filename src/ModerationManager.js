34503a90b2ae9c50d92ea8e679a72bd1636ecba3
Logan <46827071+rckymntn@users.noreply.github.com>
2023-12-10 01:04:30 -0800

    Complete stub methods

--- src/ModerationManager.js
+++ src/ModerationManager.js
@@ -1,13 +1,41 @@
+import config from "../config.json" assert { type: "json" };
+
 export default class ModerationManager {
 	constructor() {
 
 	}
 
 	isPreemptivelyBanned(added) {
-		return false;
+		if (added.user.username in config.moderation.preemptiveBanList) {
+			return true;
+		}
 	}
 
 	screen(message) {
-        return false;
+        for (word of config.moderation.filter) {
+			if (this.sanitize(message).includes(word)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	sanitize(string) {
+		string.toLowerCase();
+		string = string.replace(/['".,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+		string = string.replace(/1/g, "l");
+		string = string.replace(/3/g, "e");
+		string = string.replace(/4/g, "a");
+		string = string.replace(/5/g, "s");
+		string = string.replace(/7/g, "t");
+		string = string.replace(/8/g, "b");
+		string = string.replace(/9/g, "g");
+		string = string.replace(/0/g, "o");
+		string = string.replace(/!/g, "i");
+		string = string.replace(/@/g, "a");  
+		string = string.replace(/\$/g, "s");
+		let blah = "blahg"
+		blah.includes
+		return string;
 	}
 }
